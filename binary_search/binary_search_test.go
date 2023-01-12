2c6c7ed0ac4b49db24c9540f6e895e7f57ac4af0
dilmurodov <tolibdilmurodov98@gmail.com>
2023-01-12 10:35:11 +0500

    [*] binary search && merge_sort algo implementation

--- binary_search/binary_search_test.go
+++ binary_search/binary_search_test.go
@@ -12,49 +12,49 @@ func TestBinarySearch(t *testing.T) {
 		ok       bool
 	}{
 		{
-			name:     "Test Case 1: Positive Test Case",
+			name:     "Test 1: Positive Test Case",
 			arr:      []int{1, 2, 3, 4, 5},
 			x:        3,
 			expected: 2,
 			ok:       true,
 		},
 		{
-			name:     "Test Case 2: Negative Test Case - element not in array",
+			name:     "Test 2: Negative Test Case - element not in array",
 			arr:      []int{1, 2, 3, 4, 5},
 			x:        8,
 			expected: 5,
 			ok:       false,
 		},
 		{
-			name:     "Test Case 3: Empty Array",
+			name:     "Test 3: Empty Array",
 			arr:      []int{},
 			x:        3,
-			expected: 0,
+			expected: 1,
 			ok:       false,
 		},
 		{
-			name:     "Test Case 4: Array of length 1",
+			name:     "Test 4: Array of length 1",
 			arr:      []int{5},
 			x:        5,
 			expected: 0,
 			ok:       true,
 		},
 		{
-			name:     "Test Case 5: Positive Test Case",
+			name:     "Test 5: Positive Test Case",
 			arr:      []int{1, 2, 3, 4, 5},
 			x:        7,
 			expected: 5,
 			ok:       false,
 		},
 		{
-			name:     "Test Case 6: Large array",
+			name:     "Test 6: Large array",
 			arr:      []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30},
 			x:        15,
 			expected: 14,
 			ok:       true,
 		},
 		{
-			name:     "Test Case 7: Large array with dublicat element",
+			name:     "Test 7: Large array with dublicat element",
 			arr:      []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30},
 			x:        15,
 			expected: 14,
@@ -62,11 +62,11 @@ func TestBinarySearch(t *testing.T) {
 		},
 	}
 
-	for _, test := range testCases {
+	for ii, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			index, ok := BinarySearch(test.arr, test.x)
 			if index != test.expected || ok != test.ok {
-				t.Errorf("Test case failed: expected index=%d, ok=%t but got index=%d, ok=%t", test.expected, test.ok, index, ok)
+				t.Errorf("Test %d failed: expected index=%d, ok=%t but got index=%d, ok=%t", ii+1, test.expected, test.ok, index, ok)
 			}
 		})
 	}
@@ -82,56 +82,56 @@ func TestBinarySearchUpperBound(t *testing.T) {
 		ok       bool
 	}{
 		{
-			name:     "Test Case 1: Positive Test Case",
+			name:     "Test 1: Positive Test Case",
 			arr:      []int{1, 2, 3, 4, 5},
 			x:        3,
 			expected: 2,
 			ok:       true,
 		},
 		{
-			name:     "Test Case 2: Negative Test Case - element not in array",
+			name:     "Test 2: Negative Test Case - element not in array",
 			arr:      []int{1, 2, 3, 4, 5},
 			x:        8,
 			expected: 4,
 			ok:       false,
 		},
 		{
-			name:     "Test Case 3: Empty Array",
+			name:     "Test 3: Empty Array",
 			arr:      []int{},
 			x:        3,
 			expected: -1,
 			ok:       false,
 		},
 		{
-			name:     "Test Case 4: Array of length 1",
+			name:     "Test 4: Array of length 1",
 			arr:      []int{5},
 			x:        5,
 			expected: 0,
 			ok:       true,
 		},
 		{
-			name:     "Test Case 5: Large array",
+			name:     "Test 5: Large array",
 			arr:      []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30},
 			x:        15,
 			expected: 14,
 			ok:       true,
 		},
 		{
-			name:     "Test Case 6: Possitive Test Case with dublicates elements",
+			name:     "Test 6: Possitive Test Case with dublicates elements",
 			arr:      []int{1, 2, 3, 3, 4, 4, 4, 5},
 			x:        4,
 			expected: 6,
 			ok:       true,
 		},
 		{
-			name:     "Test Case 7: Negative Test Case with dublicates elements",
+			name:     "Test 7: Negative Test Case with dublicates elements",
 			arr:      []int{1, 2, 3, 3, 4, 4, 4, 5},
 			x:        7,
 			expected: 7,
 			ok:       false,
 		},
 		{
-			name:     "Test Case 8: Positive Test Case search element located in last index.",
+			name:     "Test 8: Positive Test Case search element located in last index.",
 			arr:      []int{1, 2, 3, 3, 4, 4, 4, 5, 5, 5},
 			x:        5,
 			expected: 9,
@@ -139,11 +139,11 @@ func TestBinarySearchUpperBound(t *testing.T) {
 		},
 	}
 
-	for _, test := range testCases {
+	for ii, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			index, ok := BinarySearchUpperBound(test.arr, test.x)
 			if index != test.expected || ok != test.ok {
-				t.Errorf("Test case failed: expected index=%d, ok=%t but got index=%d, ok=%t", test.expected, test.ok, index, ok)
+				t.Errorf("Test %d failed: expected index=%d, ok=%t but got index=%d, ok=%t", ii+1, test.expected, test.ok, index, ok)
 			}
 		})
 	}
